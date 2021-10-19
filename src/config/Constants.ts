537e581dbc072a7a1065697b8ded4069119cb481
Samuel Barbosa <samuel.barbosa1997@hotmail.com>
2021-10-19 15:02:16 -0300

    first commit

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION: 
+    'mongodb+srv://<samuel>:<12345>@url-shortener.agfky.mongodb.net/url-shortener?retryWrites=true&w=majority'
+}
